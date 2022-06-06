927ac60320f4216dc27207d346ee5586c0c8a7e7
VerrySoda <utelas@naver.com>
2022-06-07 01:22:25 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-const searchEl = document.querySelector('.search'); // document = HTML에서 querySelector(찾을것이냐)
-const searchInputEl = searchEl.querySelector('input'); // searchEl에서 찾을것이냐 input을
-
-// search라는 div요소를 click하면 function()을 실행해라
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-
-// function(){}으로 실행되는 부분을 핸들러라고 한다
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-  // html의 속성을 attribute라고 한다
-});
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-  // html의 속성을 attribute라고 한다
-});
 
 
 
@@ -189,10 +168,4 @@ spyEls.forEach(function(spyEl){  //() 메소드 {} 객체데이터
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller); //ScollMagic이라는 자바스크립트 라이브러리, Controller 메소드
 });
-
-
-const thisYear = document.querySelector('.this-year');
-  //thisYear라는 변수를 선언하고 .this-year라는 클래스를 찾아보고
-  //thisYear에 date().getFullYear();의 출력값이 .this-year 클래스에 할당이된다
-thisYear.textContent = new Date().getFullYear(); //2021이 출력됨
 
